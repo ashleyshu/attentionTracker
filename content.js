9774e7184f2d7124fc32093f1b54177aced8126a
Ashley Shu <ashu@uwo.ca>
2020-06-16 00:39:40 -0700

    Accidentally committed my kittiesGalore project

---
+++ content.js
@@ -0,0 +1,11 @@
+console.log("content script");
+
+
+// Occurs when the extension icon is clicked
+chrome.runtime.onMessage.addListener(gotMessage);
+
+function gotMessage(message, sender, sendResponse) {
+    console.log(message.txt);       // message.txt = "hello"
+    console.log("sdf");
+};
+
